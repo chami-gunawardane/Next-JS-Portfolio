cde032df950531b6515e1cd347616f21f23dfbf2
IT21356704 <it21356704@my.sliit.lk>
2024-07-20 18:07:54 +0530

    Hero section completed

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Chami's Portfolio",
+  description: "Skilled Frontend Developer proficient in HTML, CSS, JavaScript, React, and Tailwind, dedicated to crafting visually appealing and user-centric web interfaces",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}><ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider></body>
+    </html>
+  );
+}
